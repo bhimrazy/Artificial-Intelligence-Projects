da07c6c7c188bbb6da6562382b2af5319486fdd2
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2021-10-20 06:43:58 +0000

    added function to display grids of original value

--- encoding_sudoku.py
+++ encoding_sudoku.py
@@ -27,7 +27,42 @@ def chunk_string_by_len(string: str, n: int = 3) -> List[str]:
     return [string[i:i+n] for i in range(0, len(string), n)]
 
 
+def grid_values(puzzle:str,boxes:List[str],replace:bool=True) -> Dict[str,str]:
+    """This function maps each puzzle unit to its box unit.
+    Args:
+        puzzle (str): String of puzzle
+        boxes (List[str]): A list of box units
+        replace (bool) : An option to relace dots with number 1-9.Defaults to True
+    Returns:
+        Dict[str,str]: A dictionary of box units with its puzzle value.
+    """
+    return {key : ( '123456789' if value =='.' and replace else value) for key,value in zip(boxes,puzzle)}
 
+    
+def display_grid(p_values:Dict[str,str])  -> None:
+    """This function displays the dictonary in proper grid format.
+    Args:
+        p_values (Dict[str,str]): Dictionary of puzzlle with box number and value.
+    """
+    assert (len(p_values) == 81),"There must be 81 values in the dictionary."
+    
+    max_len=len(max(list(p_values.values()),key=len))+2 #max length among all box units
+    
+    print(f"\n{' SUDOKU '.center(max_len*9,'=')}\n")
+    list_puzzle = list(p_values.items())
+    
+    n=9 #step
+    for i in range(0,len(p_values),n):
+        row=''
+        for index,box in enumerate(list_puzzle[i:i+n]):
+            if (index > 1 and index < 9) and index % 3 == 0 :
+                row +='|'  #to add a pipe in middle
+            row +=box[1].center(max_len)
+            
+        print(row,'\n')
+        if i == 18 or i== 45 :  #to add a decorative line in middle 
+            pat='-'*(max_len*3) #pattern
+            print('+'.join([pat,pat,pat]),'\n')
             
   
 def main(display_units:bool=False):
@@ -48,6 +83,9 @@ def main(display_units:bool=False):
         print(f"col_units : \n{col_units}\n")
         print(f"square_units : \n{square_units}\n")
         print(f"square_units : \n{unit_list}\n")
+    
+    puzzle = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    display_grid(grid_values(puzzle,boxes,replace=False)) #display original
     
     
 if __name__ == "__main__":
